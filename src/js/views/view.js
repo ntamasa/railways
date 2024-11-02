65419b122761557d8ae8cb9d212c1ff135661c53
Tamás Nagy <ntamasalbert@gmail.com>
2024-11-02 11:23:42 +0100

    add favicon

---
+++ src/js/views/view.js
@@ -0,0 +1,38 @@
+export default class View {
+  _data;
+
+  /**
+   * Render the recieved object to the DOM
+   * @param {Object | Object[]} data The data to be rendered
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Tamás Nagy
+   */
+  render(data, render) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+
+    this._data = data;
+    const markup = this._generateMarkup();
+
+    // if (!render) return markup;
+    if (!render) {
+      this._parentElement.insertAdjacentHTML("afterbegin", markup);
+      return;
+    }
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  update(data) {}
+
+  _clear() {}
+
+  renderSpinner() {}
+
+  renderError(message = this._errorMessage) {}
+
+  renderMessage() {}
+}
